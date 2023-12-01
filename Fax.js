cc1286419ce7a94471d96ad51b5d5d9bf4cf15e8
ajaxcloud <86687902+ajaxcloud@users.noreply.github.com>
2023-12-01 13:17:18 +0530

    Delete Fax.js

--- Fax.js
+++
@@ -1,4 +0,0 @@
-//document.getElementById("foo").innerHTML = window.location;
-if(window.location !== "https://ajaxcloud.github.io/#auth=@sandystrays") {
-	window.location = "https://ajaxcloud.github.io/401.html";
-}
